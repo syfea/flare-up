6b10ba02a63bbab6bb1001e55b7c97a1f4f90218
syfea <s.feasson@gmail.com>
2016-04-22 15:35:52 +0200

    User update

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,10 +2,54 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class UserController extends Controller
 {
     public function indexAction()
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+        return $this->render('AppBundle:User:index.html.twig', array('users' => $users));
+    }
+
+    public function updateAction(User $user, Request $request)
+    {
+        $form = $this->createFormBuilder($user)
+            ->add('id', 'hidden')
+            ->add('firstname', 'text')
+            ->add('lastname', 'text')
+            ->add('username', 'text')
+            ->add('email', 'text')
+            ->add('zip_code', 'text', ['required' => false])
+            ->add('address', 'text', ['required' => false])
+            ->add('city', 'text', ['required' => false])
+            ->add('phone', 'text', ['required' => false])
+            ->add('facebook', 'text', ['required' => false])
+            ->add('twitter', 'text', ['required' => false])
+            ->add('skype', 'text', ['required' => false])
+            ->add('about_me', 'textarea')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return new RedirectResponse($this->generateUrl('app_backofficebundle_users'));
+        }
+
+        return $this->render('AppBundle:User:view.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function deleteAction(Request $request)
     {
         $userManager = $this->get('fos_user.user_manager');
         $users = $userManager->findUsers();
